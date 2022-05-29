2313a203665b4c45c84d6fe5a0a77e63b9bdd2b2
bmassioui <bouchaib.massioui@gmail.com>
2022-05-29 15:42:11 +0100

    Scaffold Login and Register pages

---
+++ src/Blazor_Authentication_Authorization.BlazorApp/Shared/LoginDisplay.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Blazor_Authentication_Authorization.BlazorApp.Shared;
+
+public partial class LoginDisplay : ComponentBase
+{
+    [Inject]
+    private IDialogService DialogService { get; set; }
+
+
+    private async Task OnLogRegisterButton_Click()
+    {
+        var options = new DialogOptions { CloseOnEscapeKey = true };
+        //_dialogService.Show<DialogUsageExample_Dialog>("Simple Dialog", options);
+    }
+}
